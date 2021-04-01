ba7f066e9148501b301dccf48a9563b1ecf1a432
yazanrayyes <73113909+yazanrayyes@users.noreply.github.com>
2021-04-01 02:22:55 -0500

    Update FirstClass.java

--- GroupProjectAdv/src/FirstClass.java
+++ GroupProjectAdv/src/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("123");
 		System.out.println("this by khaled");
+		System.out.println("this by Yazan");
 
 	}
 

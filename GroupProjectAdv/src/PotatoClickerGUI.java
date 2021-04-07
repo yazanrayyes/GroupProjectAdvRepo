03908f07b18eeab37f9a01182e1407c6a2cbe757
bugsypoopins <lordo@Jesus>
2021-04-07 13:49:09 +0300

    Ismael Commit - Very Basic Skeleton

---
+++ GroupProjectAdv/src/PotatoClickerGUI.java
@@ -0,0 +1,33 @@
+
+import java.awt.Button;
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class PotatoClickerGUI {
+	
+	JFrame frame;
+	private Button potatobutton;
+	
+	
+	
+	//Done by Yazan, Khaled, Ismail
+	// Outline 4/7
+	
+	public PotatoClickerGUI() {
+		
+		JFrame frame = new JFrame();
+		
+		// Will set image Icon here
+		
+		
+		frame.setResizable(false);
+		frame.setFocusable(true);
+		frame.setSize(500,500);
+		frame.getContentPane().setBackground(Color.BLACK);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+		
+		
+	}
+}

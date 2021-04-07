055ca63721f3ffa33bcef41c69997549cf42c5bf
bugsypoopins <lordo@Jesus>
2021-04-07 14:02:04 +0300

    Ismael commit 4/7...

---
+++ GroupProjectAdv/src/PotatoClickGUI.java
@@ -0,0 +1,33 @@
+
+import java.awt.Button;
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class PotatoClickGUI {
+	
+	JFrame frame;
+	private Button potatobutton;
+	
+	
+	
+	//Done by Yazan, Khaled, Ismail
+	// Outline 4/7
+	
+	public PotatoClickGUI() {
+		
+		JFrame frame = new JFrame();
+		
+		// Will set image Icon here
+		
+		
+		frame.setResizable(false);
+		frame.setFocusable(true);
+		frame.setSize(500,500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.setVisible(true);
+		
+		
+		
+	}
+}

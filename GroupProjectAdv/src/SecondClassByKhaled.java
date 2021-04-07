9ecc9a290a0acee687b7cbe7a3f96ae9dbc7e951
bugsypoopins <lordo@Jesus>
2021-04-07 13:25:31 +0300

    Ismael pushes yet again

--- GroupProjectAdv/src/SecondClassByKhaled.java
+++ GroupProjectAdv/src/SecondClassByKhaled.java
@@ -2,6 +2,7 @@
 public class SecondClassByKhaled {
 
 	public static void main(String[] args) {
+		System.out.print("Hello Khaled");
 		// TODO Auto-generated method stub
 		//Khaled
 	}
--- GroupProjectAdv/src/ThirdClassIsmael.java
+++ GroupProjectAdv/src/ThirdClassIsmael.java
@@ -4,6 +4,7 @@ public class ThirdClassIsmael {
 	
 	public static void main(String[] args) {
 		System.out.println("Ismael Test");
+		// HELLO BOYS
 	}
 
 }

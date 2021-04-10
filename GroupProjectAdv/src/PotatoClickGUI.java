c51b2c74b1d7db9506701855b6f4957cb265ba70
KhaledBayaydah <KhaledAlBayaydah21@21KhaledAlbayaydah.kingsacademy.edu.jo>
2021-04-10 22:25:45 +0300

    Added a potato counter by Khaled Bayaydah.

--- GroupProjectAdv/src/PotatoClickGUI.java
+++ GroupProjectAdv/src/PotatoClickGUI.java
@@ -1,8 +1,11 @@
 
 import java.awt.Button;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Image;
 import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
@@ -12,18 +15,22 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 
-public class PotatoClickGUI {
+public class PotatoClickGUI implements ActionListener {
 	
 	JFrame frame;
 	private JButton potatoButton;
 	private ImageIcon potato;
 	private JLabel image;
+	JLabel counter;
+	int potatoCounter = 0;
 	
 	
 	//Done by Yazan, Khaled, Ismail
 	// Outline 4/7
 	
 	public PotatoClickGUI() {
+		
+		potatoCounter = 0;
 		
 		JFrame frame = new JFrame();
 		// creates scaled imageIcon here
@@ -39,10 +46,16 @@ public class PotatoClickGUI {
 		potatoButton.setBackground(Color.gray);
 		potatoButton.setIcon(potato);
 		potatoButton.setBorder(null); //to avoid the blue border on potato
+		potatoButton.addActionListener(this);
 		potatoPanel.add(potatoButton);
 		frame.add(potatoPanel);
 		
+		counter=new JLabel(potatoCounter +" potatoes");
+		counter.setForeground(Color.white);
+		counter.setFont(new Font("Comic Sans Ms", Font.PLAIN, 32));
+		counter.setBounds(400,60,200,100);
 		
+		frame.add(counter);
 		frame.setResizable(false);
 		frame.getContentPane().setBackground(Color.gray);
 		frame.setLayout(null);
@@ -55,6 +68,12 @@ public class PotatoClickGUI {
 	
 	public static void main (String args[]) {
 		new PotatoClickGUI();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		potatoCounter++;
+		counter.setText(potatoCounter+ " Cookies");
 	}
 
 }

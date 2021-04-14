00200a1a91689b6c648e8376aa9e8805ef5813bb
bugsypoopins <lordo@Jesus>
2021-04-14 15:37:47 +0300

    Fixed typo and lengthened GUI for more potatoes - Ismael

--- GroupProjectAdv/src/PotatoClickGUI.java
+++ GroupProjectAdv/src/PotatoClickGUI.java
@@ -80,7 +80,7 @@ public class PotatoClickGUI implements ActionListener {
 		//creates panel and potato button
 		
 		Panel potatoPanel = new Panel();
-		potatoPanel.setBounds(250, 200, 500, 200);
+		potatoPanel.setBounds(300, 200, 500, 200);
 		potatoButton = new JButton();
 		potatoButton.setBackground(Color.gray);
 		potatoButton.setIcon(potato);
@@ -93,7 +93,7 @@ public class PotatoClickGUI implements ActionListener {
 		
 		Panel autoupgradePanel = new Panel();
 		autoupgradePanel.setBackground(Color.white);
-		autoupgradePanel.setBounds(40, 40, 200, 200);
+		autoupgradePanel.setBounds(40, 40, 250, 200);
 		autoupgradePanel.setLayout(new GridLayout(4,1));
 		
 		JLabel autoupgradelabel = new JLabel("Clickers:");
@@ -118,7 +118,7 @@ public class PotatoClickGUI implements ActionListener {
 		
 		Panel clickupgradePanel = new Panel();
 		clickupgradePanel.setBackground(Color.white);
-		clickupgradePanel.setBounds(40, 300, 200, 200);
+		clickupgradePanel.setBounds(40, 300, 250, 200);
 		clickupgradePanel.setLayout(new GridLayout(4,1));
 		
 		JLabel clickupgradelabel = new JLabel("Upgrades:");
@@ -135,7 +135,7 @@ public class PotatoClickGUI implements ActionListener {
 		clickupgrade2 = new JButton("100 Potatoes (" + plushundredqty + ") [" + plushundredprice + "p]");
 		clickupgrade2.addActionListener(this);
 		clickupgradePanel.add(clickupgrade2);
-		clickupgrade3 = new JButton("100 Potatoes (" + plusthousandqty + ") [" + plusthousandprice + "p]");
+		clickupgrade3 = new JButton("1000 Potatoes (" + plusthousandqty + ") [" + plusthousandprice + "p]");
 		clickupgrade3.addActionListener(this);
 		clickupgradePanel.add(clickupgrade3);
 		
@@ -144,21 +144,21 @@ public class PotatoClickGUI implements ActionListener {
 		counter=new JLabel(potatoCounter +" potatoes");
 		counter.setForeground(Color.white);
 		counter.setFont(new Font("Comic Sans Ms", Font.PLAIN, 32));
-		counter.setBounds(400,60,200,100);
+		counter.setBounds(450,60,200,100);
 		
 		// counter counts the number of potatoes gathered throughout the game
 		
 		lifetimecounter=new JLabel("total potatoes gathered: " + lifetimepotatoCounter);
 		lifetimecounter.setForeground(Color.white);
 		lifetimecounter.setFont(new Font("Comic Sans Ms", Font.PLAIN, 16));
-		lifetimecounter.setBounds(300,500,250,100);
+		lifetimecounter.setBounds(350,500,250,100);
 		
 		// CURRENTLY NON-FUNCTIONAL will display potatoes gathered per second as the player idles
 		
 		idlecounter=new JLabel("Test");
 		idlecounter.setForeground(Color.white);
 		idlecounter.setFont(new Font("Comic Sans Ms", Font.PLAIN, 16));
-		idlecounter.setBounds(400,100,200,100);
+		idlecounter.setBounds(450,100,200,100);
 		
 		// adds all the GUI to our frame
 		
@@ -171,7 +171,7 @@ public class PotatoClickGUI implements ActionListener {
 		frame.getContentPane().setBackground(Color.gray);
 		frame.setLayout(null);
 		frame.setFocusable(true);
-		frame.setSize(620,620);
+		frame.setSize(1000,620);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 			
